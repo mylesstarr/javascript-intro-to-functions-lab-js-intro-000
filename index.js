ecf65ceccb2e8e776818673f9a98ecd428c68512
Myles Starr <mylesstarr@gmail.com>
2021-03-22 22:59:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if string.tolowercase() === string
+    return "I can't hear you!"
+  else if string.toUpperCase() === string
+    return "YES INDEED!"
+  else if string === "I love you, Grandma."
+    retrun "I love you, too."
+  }   
